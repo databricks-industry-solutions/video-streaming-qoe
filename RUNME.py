8395130e556769eda9458fd3026b1fc619bd866e
dbbnicole <dbbnicole>
2022-10-14 14:32:44 +0000

    Update driver result limit and cluster VM type to avoid OOM

--- RUNME.py
+++ RUNME.py
@@ -85,10 +85,10 @@ job_json = {
                 "new_cluster": {
                     "spark_version": "10.4.x-cpu-ml-scala2.12",
                 "spark_conf": {
-                    "spark.databricks.delta.formatCheck.enabled": "false"
+                    "spark.driver.maxResultSize": "16g"
                     },
-                    "num_workers": 2,
-                    "node_type_id": {"AWS": "i3.xlarge", "MSA": "Standard_D3_v2", "GCP": "n1-highmem-4"},
+                    "num_workers": 1,
+                    "node_type_id": {"AWS": "i3.2xlarge", "MSA": "Standard_DS4_v2", "GCP": "n1-highmem-8"},
                     "custom_tags": {
                         "usage": "solacc_testing"
                     }
@@ -99,7 +99,9 @@ job_json = {
 
 # COMMAND ----------
 
-NotebookSolutionCompanion().deploy_compute(job_json)
+dbutils.widgets.dropdown("run_job", "False", ["True", "False"])
+run_job = dbutils.widgets.get("run_job") == "True"
+NotebookSolutionCompanion().deploy_compute(job_json, run_job=run_job)
 
 # COMMAND ----------
 
